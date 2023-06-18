940f6db11fa8c870e786679a2160feb5839ac756
igarashi20176 <c0a20176bc@edu.teu.ac.jp>
2023-06-18 16:29:28 +0900

    initialize

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Inter } from 'next/font/google';
+import styles from '@/src/styles/Home.module.css';
+
+export default function Home() {
+  const hello = 'hello js';
+
+  return (
+    <>
+      <div>
+        <p>Hello</p>
+      </div>
+    </>
+  );
+}
