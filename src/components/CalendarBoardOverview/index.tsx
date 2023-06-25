f4dccd984fac063fadb77df3f43ef9f1ed285160
igarashi20176 <c0a20176bc@edu.teu.ac.jp>
2023-06-25 17:30:02 +0900

    OverviewCalendarをコンポーネント化

--- src/components/CalendarBoardOverview/index.tsx
+++ src/components/CalendarBoardOverview/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './CalendarBoardOverview.module.css';
 
 import { NavigationArrow } from '@/components/elements/NavigationArrow';
+import { OverviewCalendar } from '@/components/elements/OverviewCalendar';
 
 export const CalendarBoardOverview = () => {
   const days: Array<string> = ['日', '月', '火', '水', '木', '金', '土'];
@@ -9,27 +10,8 @@ export const CalendarBoardOverview = () => {
   return (
     <div className={styles.overview}>
       <div className={styles.overview_calendar}>
-        <div className={styles.overview_calendar_navigation}>
-          <h3>2023 年 6 月 1 日</h3>
-          <div className={styles.overview_arrow}>
-            <NavigationArrow size='medium' />
-          </div>
-        </div>
-        <div className={styles.overview_calendar_elements}>
-          <div className={styles.week}>
-            {days.map((day) => {
-              return <h4 key={day}>{day}</h4>;
-            })}
-          </div>
-          <div className={styles.elements}>
-            {new Array(35).fill(0).map((_, i) => {
-              return (
-                <h4 key={i} className={styles.element}>
-                  {i}
-                </h4>
-              );
-            })}
-          </div>
+        <div className={styles.calendar_container}>
+          <OverviewCalendar />
         </div>
       </div>
       <div className={styles.overview_options}>
