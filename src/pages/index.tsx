4add193383d93f17c223a7ba54e4b54661a66811
igarashi20176 <c0a20176bc@edu.teu.ac.jp>
2023-06-30 01:14:14 +0900

    index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,56 @@
 import { Inter } from 'next/font/google';
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 import styles from '@/styles/Home.module.css';
+
+import { CalendarContextType } from '@/types';
 
 import { Header } from '@/components/layouts/Header';
 import { CalendarBoard } from '@/components/CalendarBoard';
 
+export const CalendarContext = createContext<CalendarContextType>({
+  year: 2023,
+  month: 6,
+  board: [],
+});
+
+const getCalendarBoard = (currYear: number, currMonth: number): number[] => {
+  const calendarBoard: number[] = Array(35)
+    .fill(0)
+    .map((_, i) => {
+      const firstDate = new Date(currYear, currMonth, 1);
+      const firstDay = firstDate.getDay();
+      const curDay = i - firstDay;
+
+      firstDate.setDate(firstDate.getDate() + curDay);
+      const calendarDate = firstDate.getDate();
+      return calendarDate;
+    });
+
+  return calendarBoard;
+};
+
 export default function Home() {
-  const [now_date, set_now_date] = useState(new Date());
+  const today = new Date();
+  const [currYear, currMonth, currDate] = [today.getFullYear(), today.getMonth(), today.getDate()];
+
+  const calendarBoard: number[] = getCalendarBoard(currYear, currMonth);
 
   return (
     <div className={styles.container}>
-      <div className={styles.container_header}>
-        <Header />
-      </div>
-      <div className={styles.container_board}>
-        <CalendarBoard />
-      </div>
+      <CalendarContext.Provider
+        value={{
+          year: currYear,
+          month: currMonth,
+          board: calendarBoard,
+        }}
+      >
+        <div className={styles.container_header}>
+          <Header />
+        </div>
+        <div className={styles.container_board}>
+          <CalendarBoard />
+        </div>
+      </CalendarContext.Provider>
     </div>
   );
 }
